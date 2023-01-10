d5cc500352df9914d9d46326249c9cb49bdc5787
devyouth94 <devyouth94@gmail.com>
2023-01-10 22:35:20 +0900

    Feat: contentlayer 설정

---
+++ contentlayer.config.ts
@@ -0,0 +1,25 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: true },
+    date: { type: 'string', required: true },
+    summary: { type: 'string', required: true },
+    tags: { type: 'list', required: true, of: { type: 'string' }, default: [] },
+  },
+  computedFields: {
+    slug: {
+      type: 'string',
+      resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ''),
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: 'posts',
+  documentTypes: [Post],
+  mdx: {},
+});
