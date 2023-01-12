6979d1adc20fbbbfb4045a8367cd0b8d9e90fb09
devyouth94 <devyouth94@gmail.com>
2023-01-12 14:50:49 +0900

    Style: 메인 페이지 소개글 미디어쿼리 사이즈 수정

--- components/IndexProfile.tsx
+++ components/IndexProfile.tsx
@@ -4,14 +4,14 @@ import { TypeAnimation } from 'react-type-animation';
 
 const IndexProfile = () => {
   return (
-    <div className="flex justify-between mb-5">
+    <div className="grid grid-cols-[auto_120px] sm:grid-cols-[auto_180px] mb-5">
       <div>
         <TypeAnimation
           sequence={[`안녕하세요! 김영진입니다.`, 1000, '', 500]}
           speed={10}
           repeat={Infinity}
           wrapper="h1"
-          className="text-3xl font-bold"
+          className="text-xl sm:text-3xl font-bold"
         />
         <p className="mt-3 font-sm leading-7 text-neutral-500 dark:text-neutral-200 post-title">
           <span className="post-underline">
@@ -24,7 +24,10 @@ const IndexProfile = () => {
           공유하고 싶은 글을 다듬어 이 곳에 올립니다.
         </p>
       </div>
-      <Image src="/images/mimoji.png" width={180} height={180} alt="mimoji" />
+
+      <div className="relative h-[120px] sm:h-[180px]">
+        <Image src="/images/mimoji.png" fill alt="mimoji" />
+      </div>
     </div>
   );
 };
