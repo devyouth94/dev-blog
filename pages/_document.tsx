f3b3e786b7eb39d9dd811c500695d19912857bb0
devyouth94 <devyouth94@gmail.com>
2023-02-22 13:30:54 +0900

    Fix: 구글 서치 콘솔 등록 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,10 @@ export default function Document() {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
         <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
+        <meta
+          name="google-site-verification"
+          content="p15ksZCsN_bZNG0qLoBHODbLqGysOQMu884I02V2ebM"
+        />
       </Head>
 
       <body className="bg-gray-50 text-gray-900 dark:bg-neutral-800 dark:text-gray-100">
